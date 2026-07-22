548b70d571fe84f8d4d67d0275b97984e08e1ee4
zubairudalhatu <zubairudalhatu@users.noreply.github.com>
2026-07-22 17:03:36 +0100

    chore: bump customer android version code to 9

--- apps/customer-app/app.config.ts
+++ apps/customer-app/app.config.ts
@@ -48,7 +48,7 @@ export default ({ config }: { config: Record<string, any> }) => ({
       backgroundColor: "#FFFFFF"
     },
     package: isStaging ? "com.karigo.customer.staging" : "com.karigo.customer",
-    versionCode: isStaging ? 1 : 8
+    versionCode: isStaging ? 1 : 9
   },
   ios: {
     ...config.ios,
